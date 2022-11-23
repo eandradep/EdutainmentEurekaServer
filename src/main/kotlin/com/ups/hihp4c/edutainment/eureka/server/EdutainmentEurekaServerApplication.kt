d361ec2679b92530e8d54145d1655a3d4d39de65
edisonAndrade1994 <edison.andrade@weconnect.com.ec>
2022-11-22 19:14:36 -0500

    Initial commit

---
+++ src/main/kotlin/com/ups/hihp4c/edutainment/eureka/server/EdutainmentEurekaServerApplication.kt
@@ -0,0 +1,11 @@
+package com.ups.hihp4c.edutainment.eureka.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdutainmentEurekaServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EdutainmentEurekaServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/ups/hihp4c/edutainment/eureka/server/EdutainmentEurekaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ups.hihp4c.edutainment.eureka.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdutainmentEurekaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
